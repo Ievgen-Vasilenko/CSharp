103a48a7d2fb7eff75a2885788cdc286a3bb059d
Ievgen-Vasilenko <e.wasel1990@gmail.com>
2020-02-08 11:26:58 +0200

    A simple application for accounting finance, based on the new knowledge of OOP.

---
+++ Lessons/Lesson3/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Lesson3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MoneyEntry me = new MoneyEntry();
+
+            Console.WriteLine("Hello!");
+
+            Console.Write("Enter amount: ");
+
+            string amount = Console.ReadLine();
+
+            Console.Write("Enter date: ");
+
+            string date = Console.ReadLine();
+
+            me.InitWithString(amount, date);
+
+            Console.WriteLine("Your record:");
+
+            Console.WriteLine(me.ToString());
+
+            if (me.isDebit)
+                Console.WriteLine("You have a new income.");
+            else
+                Console.WriteLine("You have a new expense.");
+        }
+    }
+}
