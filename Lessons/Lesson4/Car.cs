4779b71a63c405356888a2e83afac5c7b39d0750
Ievgen-Vasilenko <e.wasel1990@gmail.com>
2020-02-15 11:43:17 +0200

    A small OOP application that uses scope modifiers for methods and values.

---
+++ Lessons/Lesson4/Car.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lesson4
+{
+    public abstract class Car
+    {
+        protected int _speed;
+        protected string _type;
+
+        public Car()
+        {
+            _speed = 0;
+            _type = "noname";
+        }
+
+        public abstract void Drive();
+
+        public override string ToString()
+        {
+            return string.Format("Model: {0}; Max speed: {1}", _type, _speed);
+        }
+
+    }
+
+    public class Vaz : Car
+    {
+
+        public Vaz() :base()
+        {
+            _type = "VAZ 2107";
+        }
+
+        public override void Drive()
+        {
+            _speed = 60;
+        }
+
+    }
+
+    public class Lamborgini : Car, ITurbo
+    {
+        public Lamborgini() : base()
+        {
+            Turbo();
+            _type = "Gallardo";
+        }
+
+        public override void Drive()
+        {
+            _speed = 260;
+        }
+
+        public void Turbo()
+        {
+            
+        }
+
+    }
+
+    public class Ferrari : Car, IExtendedEngine
+    {
+
+        public Ferrari() : base()
+        {
+            _type = "Testarossa";
+        }
+
+        public override void Drive()
+        {
+            ExtendEngine();
+            _speed = 240;
+        }
+
+        public void ExtendEngine()
+        {
+            
+        }
+
+    }
+
+    interface ITurbo
+    {
+        void Turbo();
+    }
+
+    interface IExtendedEngine
+    {
+        void ExtendEngine();
+    }
+}
---
+++ Lessons/Lesson4/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Lesson4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter vehicle type:");
+            Console.WriteLine("VAZ - to select VAZ 2107");
+            Console.WriteLine("Lamborgini - to select Labmborgini Gallardo");
+            Console.WriteLine("Ferrari - to select Ferrari Testarossa");
+
+            string type = Console.ReadLine();
+
+            Car choosenCar = GetCarByType(type);
+            Console.WriteLine();
+            Console.WriteLine("You choosed:");
+            Console.WriteLine(choosenCar.ToString());
+
+            Console.WriteLine("Lets GO!:");
+            choosenCar.Drive();
+            Console.WriteLine(choosenCar.ToString());
+
+            if(choosenCar is ITurbo)
+            {
+                Console.WriteLine("Turbocharged car");
+                ((Lamborgini)choosenCar).Turbo();
+            }
+
+            Console.ReadKey();
+        }
+
+        static Car GetCarByType(string type)
+        {
+            switch (type)
+            {
+                case "VAZ":
+                    return new Vaz();
+                case "Lamborgini":
+                    return new Lamborgini();
+                case "Ferrari":
+                    return new Ferrari();
+                default:
+                    return null;
+            }
+        }
+    }
+}
